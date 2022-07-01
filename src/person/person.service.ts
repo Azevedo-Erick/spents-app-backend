8bdb29865753889c155dee54f9ae84f6a1fb5f59
Azevedo-Erick <azevedoerick762@gmail.com>
2022-07-01 13:21:34 -0300

    FEAT: person CRUD complete

--- src/person/person.service.ts
+++ src/person/person.service.ts
@@ -1,28 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreatePersonDto } from './dto/create-person.dto';
 import { UpdatePersonDto } from './dto/update-person.dto';
-
+import { hashSync } from 'bcrypt';
 @Injectable()
 export class PersonService {
-  constructor(private readonly prisma:PrismaService){}
+  constructor(private readonly prisma: PrismaService) { }
   create(createPersonDto: CreatePersonDto) {
-    return 'This action adds a new person';
+    createPersonDto.password = hashSync(createPersonDto.password, 10);
+    try {
+      return this.prisma.person.create({
+        data: {
+          ...createPersonDto
+        }
+      })
+    } catch (e) {
+      throw new InternalServerErrorException({ message: 'Error creating person' });
+    }
   }
 
   findAll() {
-    return `This action returns all person`;
+    try {
+      return this.prisma.person.findMany();
+    } catch (e) {
+      throw new InternalServerErrorException({ message: 'Error finding all persons' });
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} person`;
+  findOne(id: string) {
+    try {
+      this.prisma.person.findFirst({
+        where: {
+          id: id
+        }
+      })
+    } catch (e) {
+      throw new InternalServerErrorException({ message: 'Error finding one person' });
+    }
   }
 
-  update(id: number, updatePersonDto: UpdatePersonDto) {
-    return `This action updates a #${id} person`;
+  update(id: string, updatePersonDto: UpdatePersonDto) {
+    try {
+      this.prisma.person.update({
+        data: {
+          ...updatePersonDto
+        },
+        where: {
+          id: id
+        }
+      })
+    } catch (e) {
+      throw new InternalServerErrorException({ message: 'Error updating person' });
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} person`;
+  remove(id: string) {
+    try {
+      this.prisma.person.delete({
+        where: {
+          id: id
+        }
+      })
+    } catch (e) {
+      throw new InternalServerErrorException({ message: 'Error removing person' });
+    }
   }
 }
